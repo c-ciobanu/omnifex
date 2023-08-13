600ae8bd9e8d51a197c485e65917f701e9023ea1
Cristi <c-ciobanu@protonmail.com>
2023-08-13 19:27:47 +0100

    add useLocalStorage hook

---
+++ web/src/hooks/useLocalStorage/useLocalStorage.test.ts
@@ -0,0 +1,45 @@
+import { renderHook, waitFor } from '@redwoodjs/testing/web'
+
+import { useLocalStorage } from './useLocalStorage'
+
+describe('useLocalStorage', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  it('returns the default value', () => {
+    const defaultValue = { a: false, b: false }
+
+    const { result } = renderHook(() =>
+      useLocalStorage('testObject', defaultValue)
+    )
+
+    expect(result.current[0]).toEqual(defaultValue)
+  })
+
+  it('returns the value stored in the local storage', () => {
+    const key = 'testObject'
+    const localStorageValue = { a: true, b: true }
+
+    localStorage.setItem(key, JSON.stringify(localStorageValue))
+
+    const { result } = renderHook(() =>
+      useLocalStorage(key, { a: false, b: false })
+    )
+
+    expect(result.current[0]).toEqual(localStorageValue)
+  })
+
+  it('sets a new value', async () => {
+    const defaultValue = 'before'
+    const newValue = 'after'
+
+    const { result } = renderHook(() =>
+      useLocalStorage('testString', defaultValue)
+    )
+
+    expect(result.current[0]).toBe(defaultValue)
+    await waitFor(() => result.current[1](newValue))
+    expect(result.current[0]).toBe(newValue)
+  })
+})
---
+++ web/src/hooks/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react'
+
+const getLocalStorageValue = <T>(key: string, defaultValue: T): T => {
+  const savedLocalStorageValue = localStorage.getItem(key)
+  const value = JSON.parse(savedLocalStorageValue)
+
+  return value || defaultValue
+}
+
+export const useLocalStorage = <T>(
+  key: string,
+  defaultValue: T
+): [T, Dispatch<SetStateAction<T>>] => {
+  const [value, setValue] = useState(() =>
+    getLocalStorageValue(key, defaultValue)
+  )
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value])
+
+  return [value, setValue]
+}
