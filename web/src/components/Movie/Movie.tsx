8e4d9796b5bc0419753288a79a656a1f79806575
Cristi <c-ciobanu@protonmail.com>
2023-08-14 11:29:00 +0100

    allow to set movie as favorited and watched

--- web/src/components/Movie/Movie.tsx
+++ web/src/components/Movie/Movie.tsx
@@ -1,6 +1,16 @@
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEye as faRegularEye,
+  faHeart as faRegularHeart,
+} from '@fortawesome/free-regular-svg-icons'
+import {
+  faEye as faSolidEye,
+  faHeart as faSolidHeart,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { DetailedMovie } from 'types/graphql'
+
+import { useLocalStorage } from 'src/hooks/useLocalStorage/useLocalStorage'
 
 export const formatMinutesToHoursAndMinutes = (minutes: number) => {
   const h = Math.floor(minutes / 60)
@@ -14,6 +24,11 @@ type MovieProps = {
 }
 
 const Movie = ({ movie }: MovieProps) => {
+  const [userMovieStats, setUserMovieStats] = useLocalStorage(
+    movie.id.toString(),
+    { isFavorited: false, isWatched: false }
+  )
+
   return (
     <>
       <h2 className="text-xl">{movie.title}</h2>
@@ -24,7 +39,7 @@ const Movie = ({ movie }: MovieProps) => {
         <span className="text-lg font-bold text-white">{movie.rating}</span>/10
       </h4>
 
-      <div className="mt-4 flex items-start gap-2">
+      <div className="mt-4 flex items-start gap-4">
         <img
           src={movie.posterUrl}
           alt={`${movie.title} poster`}
@@ -34,6 +49,38 @@ const Movie = ({ movie }: MovieProps) => {
         <div className="space-y-2">
           <p>{movie.genres.join(', ')}</p>
           <p>{movie.overview}</p>
+
+          <div className="space-x-4">
+            <button
+              className="h-11 w-11 rounded-full bg-neutral-800 text-xl"
+              onClick={() =>
+                setUserMovieStats((prevState) => ({
+                  ...prevState,
+                  isWatched: !prevState.isWatched,
+                }))
+              }
+            >
+              <FontAwesomeIcon
+                icon={userMovieStats.isWatched ? faSolidEye : faRegularEye}
+              />
+            </button>
+
+            <button
+              className="h-11 w-11 rounded-full bg-neutral-800 text-xl"
+              onClick={() =>
+                setUserMovieStats((prevState) => ({
+                  ...prevState,
+                  isFavorited: !prevState.isFavorited,
+                }))
+              }
+            >
+              <FontAwesomeIcon
+                icon={
+                  userMovieStats.isFavorited ? faSolidHeart : faRegularHeart
+                }
+              />
+            </button>
+          </div>
         </div>
       </div>
     </>
