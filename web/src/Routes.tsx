c3d2125e31493a845b1ba4d3bf6f95a1bc86b72e
Cristi <c-ciobanu@protonmail.com>
2023-08-14 18:52:37 +0100

    setup db auth

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -11,9 +11,11 @@ import { Router, Route, Set } from '@redwoodjs/router'
 
 import AppLayout from 'src/layouts/AppLayout/AppLayout'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>
+    <Router useAuth={useAuth}>
       <Set wrap={AppLayout}>
         <Route path="/movies/{id:Int}" page={MoviePage} name="movie" />
         <Route path="/" page={HomePage} name="home" />
