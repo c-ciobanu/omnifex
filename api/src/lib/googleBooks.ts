25bfa6bc6cff676acced46df5f79dbb94f107937
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-04-06 10:49:10 +0100

    use googleapis package for the google books lib

--- api/src/lib/googleBooks.ts
+++ api/src/lib/googleBooks.ts
@@ -1,70 +1,18 @@
-import { fetch } from '@whatwg-node/fetch'
+import { books } from '@googleapis/books'
 
-interface GoogleBook {
-  kind: 'books#volume'
-  id: string
-  etag: string
-  selfLink: string
-  volumeInfo: {
-    title: string
-    subtitle?: string
-    authors: string[]
-    publisher: string
-    publishedDate: string
-    description?: string
-    industryIdentifiers: {
-      type: string
-      identifier: string
-    }[]
-    pageCount?: number
-    dimensions: {
-      height: string
-      width: string
-      thickness: string
-    }
-    printType: string
-    mainCategory?: string
-    categories?: string[]
-    averageRating?: number
-    ratingsCount: number
-    contentVersion: string
-    imageLinks?: {
-      smallThumbnail: string
-      thumbnail: string
-      small: string
-      medium: string
-      large: string
-      extraLarge: string
-    }
-    language: string
-    previewLink: string
-    infoLink: string
-    canonicalVolumeLink: string
-  }
-}
+const booksAPI = books({
+  version: 'v1',
+  auth: process.env.GOOGLE_BOOKS_API_KEY,
+})
 
 export const searchGoogleBooks = async ({ title }: { title: string }) => {
-  const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?${new URLSearchParams({
-      q: title,
-      printType: 'books',
-      maxResults: '18',
-      orderBy: 'relevance',
-      key: process.env.GOOGLE_BOOKS_API_KEY,
-    })}`,
-    { method: 'GET', headers: { accept: 'application/json' } }
-  )
-  const json: { items: GoogleBook[] } = await response.json()
+  const response = await booksAPI.volumes.list({ q: title, printType: 'books', maxResults: 18, orderBy: 'relevance' })
 
-  return json.items
+  return response.data.items
 }
 
 export const getGoogleBook = async (googleId: string) => {
-  const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes/${googleId}?key=${process.env.GOOGLE_BOOKS_API_KEY}`,
-    { method: 'GET', headers: { accept: 'application/json' } }
-  )
-  const json: GoogleBook = await response.json()
+  const response = await booksAPI.volumes.get({ volumeId: googleId })
 
-  return json
+  return response.data
 }
