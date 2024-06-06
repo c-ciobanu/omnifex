962935a99d75da0cb27777d7b3c9fd68a5482923
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-06-06 07:35:31 +0100

    use MINIO_PORT variable only in development

--- api/src/lib/minio.ts
+++ api/src/lib/minio.ts
@@ -2,7 +2,7 @@ import * as Minio from 'minio'
 
 const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_API_URL,
-  port: Number(process.env.MINIO_PORT),
+  port: process.env.NODE_ENV !== 'development' ? undefined : Number(process.env.MINIO_PORT),
   useSSL: process.env.NODE_ENV !== 'development',
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
