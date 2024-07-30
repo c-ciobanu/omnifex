6bfa9c3fb872b69e93d8dbb6e47217e21a3f3ac8
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-07-30 13:42:27 +0200

    remove unused transitionProperty

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      transitionProperty: {
-        width: 'width',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
