b4a1fb085477f358fc3aa927d98d8b46703055bf
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-07-31 12:35:47 +0200

    remove FE documents feature code temporarily

--- web/src/pages/DocumentsPage/DocumentsPage.tsx
+++ web/src/pages/DocumentsPage/DocumentsPage.tsx
@@ -1,23 +1,11 @@
 import { Metadata } from '@redwoodjs/web'
 
-import { Editor } from './Editor/Editor'
-import { EditorProvider } from './EditorContext/EditorContext'
-import { Sidebar } from './Sidebar/Sidebar'
-
 const DocumentsPage = () => {
   return (
     <>
       <Metadata title="Documents" />
 
-      <EditorProvider>
-        <div className="flex gap-8">
-          <Sidebar />
-
-          <div className="flex-1">
-            <Editor />
-          </div>
-        </div>
-      </EditorProvider>
+      <p>This feature will be coming back soon.</p>
     </>
   )
 }
