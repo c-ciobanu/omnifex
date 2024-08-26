44d0f24ee433b081c4237ff59c342a07c1ddf930
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-08-26 09:43:56 +0200

    update docs editor theme

--- web/src/pages/DocumentPage/DocumentCell/LexicalEditor/LexicalEditor.tsx
+++ web/src/pages/DocumentPage/DocumentCell/LexicalEditor/LexicalEditor.tsx
@@ -55,6 +55,16 @@ const editorConfig = {
   theme: {
     ltr: 'text-left',
     rtl: 'text-right',
+    heading: {
+      h1: 'text-4xl font-extrabold',
+      h2: 'text-3xl font-bold',
+      h3: 'text-2xl font-semibold',
+      h4: 'text-xl font-semibold',
+      h5: 'text-lg font-semibold',
+      h6: 'font-semibold',
+    },
+    quote: 'my-6 border-l-4 border-l-gray-200 pl-4 font-medium italic text-gray-900',
+    hr: 'my-8 border-t border-gray-200',
     text: {
       bold: 'font-bold',
       code: 'bg-slate-100 px-1 py-px font-[Menlo,Consolas,Monaco,monospace] text-[90%]',
@@ -65,11 +75,11 @@ const editorConfig = {
     },
     link: 'text-blue-500 no-underline	hover:cursor-pointer hover:underline',
     list: {
-      listitem: 'mx-8 my-0',
+      listitem: 'mx-8 my-0 leading-7',
       listitemChecked:
-        'relative !ml-3 list-none pl-[26px] line-through outline-none before:absolute before:left-0 before:h-full before:cursor-pointer before:pt-1 before:content-[url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiMzYjgyZjYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1zcXVhcmUtY2hlY2stYmlnIj48cGF0aCBkPSJNMjEgMTAuNVYxOWEyIDIgMCAwIDEtMiAySDVhMiAyIDAgMCAxLTItMlY1YTIgMiAwIDAgMSAyLTJoMTIuNSIvPjxwYXRoIGQ9Im05IDExIDMgM0wyMiA0Ii8+PC9zdmc+)]',
+        'relative ml-3 list-none pl-[26px] line-through outline-none before:absolute before:left-0 before:h-full before:cursor-pointer before:pt-1 before:content-[url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiMzYjgyZjYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1zcXVhcmUtY2hlY2stYmlnIj48cGF0aCBkPSJNMjEgMTAuNVYxOWEyIDIgMCAwIDEtMiAySDVhMiAyIDAgMCAxLTItMlY1YTIgMiAwIDAgMSAyLTJoMTIuNSIvPjxwYXRoIGQ9Im05IDExIDMgM0wyMiA0Ii8+PC9zdmc+)]',
       listitemUnchecked:
-        'relative !ml-3 list-none pl-[26px] outline-none before:absolute before:left-0 before:h-full before:cursor-pointer before:pt-1 before:content-[url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNxdWFyZSI+PHJlY3Qgd2lkdGg9IjE4IiBoZWlnaHQ9IjE4IiB4PSIzIiB5PSIzIiByeD0iMiIvPjwvc3ZnPg==)]',
+        'relative ml-3 list-none pl-[26px] outline-none before:absolute before:left-0 before:h-full before:cursor-pointer before:pt-1 before:content-[url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNxdWFyZSI+PHJlY3Qgd2lkdGg9IjE4IiBoZWlnaHQ9IjE4IiB4PSIzIiB5PSIzIiByeD0iMiIvPjwvc3ZnPg==)]',
       nested: {
         listitem: 'list-none before:content-none',
       },
@@ -98,7 +108,7 @@ const LexicalEditor = (props: LexicalEditorProps) => {
         <RichTextPlugin
           contentEditable={
             <ContentEditable
-              className="prose min-h-[180px] max-w-none resize-none px-3 py-4 outline-none"
+              className="min-h-[180px] max-w-none resize-none px-3 py-4 outline-none"
               aria-placeholder={placeholder}
               placeholder={
                 <div className="pointer-events-none absolute left-3 top-4 inline-block select-none overflow-hidden text-ellipsis text-muted-foreground">
