ecbd46fdec3187f3a8ce341633cada76789217fe
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-09-20 17:33:03 +0200

    backfill default lists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 yarn dlx prisma migrate deploy --schema=/home/node/app/api/db/schema.prisma
+yarn dlx @redwoodjs/cli-data-migrate --import-db-client-from-dist
 
 node_modules/.bin/rw-jobs start
 node_modules/.bin/rw-server api
