c4ee13e9f1fc3187875c590f960145a2055f9211
Cristi Ciobanu <c-ciobanu@protonmail.com>
2024-10-07 21:49:02 +0200

    use web worker for the pomodoro timer interval

--- web/src/pages/PomodoroPage/PomodoroTimer/PomodoroTimer.tsx
+++ web/src/pages/PomodoroPage/PomodoroTimer/PomodoroTimer.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { Metadata } from '@redwoodjs/web'
 
 import { Card, CardContent, CardDescription, CardHeader } from 'src/components/ui/card'
-import { useInterval } from 'src/hooks/useInterval/useInterval'
 
 import alarm from './alarm.mp3'
 import end from './end.mp3'
@@ -42,49 +41,64 @@ const PomodoroTimer = ({ settings }: PomodoroTimerProps) => {
   const [currentPhase, setCurrentPhase] = useState(Phase.Pomodoro)
   const [secondsLeft, setSecondsLeft] = useState(settings.pomodoro * 60)
   const [runningPhase, setRunningPhase] = useState(1)
+  const workerRef = useRef<Worker>()
+  const tickRef = useRef(tick)
 
-  useInterval(
-    () => {
-      const nextPhase = runningPhase === 7 ? Phase.LongBreak : Phase.ShortBreak
-      const secondsToNextPhase = secondsLeft - 1
+  useEffect(() => {
+    tickRef.current = tick
+  })
 
-      if (runningPhase === 12 && secondsToNextPhase === 0) {
-        setSecondsLeft(0)
+  useEffect(() => {
+    workerRef.current = new Worker(new URL('./worker', import.meta.url), { type: 'module' })
 
-        sendNotification('You did it!', 'You made it to the end! Keep up the great work! 💪')
-        endSound.play()
-      } else if (secondsToNextPhase === 0) {
-        if (currentPhase === Phase.Pomodoro) {
-          setCurrentPhase(nextPhase)
-          setSecondsLeft((nextPhase === Phase.LongBreak ? settings.longBreak : settings.shortBreak) * 60)
+    workerRef.current.onmessage = () => tickRef.current()
 
-          sendNotification('Well done!', `Time to take a ${nextPhase.toLowerCase()} now.`)
-        } else {
-          setCurrentPhase(Phase.Pomodoro)
-          setSecondsLeft(settings.pomodoro * 60)
+    return () => {
+      workerRef.current?.terminate()
+    }
+  }, [])
 
-          sendNotification('Hope you are well rested now!', `It's time to go at it again.`)
-        }
+  function tick() {
+    const nextPhase = runningPhase === 7 ? Phase.LongBreak : Phase.ShortBreak
+    const secondsToNextPhase = secondsLeft - 1
 
-        setRunningPhase((state) => state + 1)
+    if (runningPhase === 12 && secondsToNextPhase === 0) {
+      workerRef.current?.terminate()
 
-        alarmSound.play()
+      setSecondsLeft(0)
+
+      sendNotification('You did it!', 'You made it to the end! Keep up the great work! 💪')
+      endSound.play()
+    } else if (secondsToNextPhase === 0) {
+      if (currentPhase === Phase.Pomodoro) {
+        setCurrentPhase(nextPhase)
+        setSecondsLeft((nextPhase === Phase.LongBreak ? settings.longBreak : settings.shortBreak) * 60)
+
+        sendNotification('Well done!', `Time to take a ${nextPhase.toLowerCase()} now.`)
       } else {
-        setSecondsLeft(secondsToNextPhase)
+        setCurrentPhase(Phase.Pomodoro)
+        setSecondsLeft(settings.pomodoro * 60)
 
-        if (currentPhase === Phase.Pomodoro && secondsToNextPhase === 300) {
-          sendNotification('Pomodoro ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 5 minutes.`)
-        } else if (currentPhase === Phase.Pomodoro && secondsToNextPhase === 60) {
-          sendNotification('Pomodoro ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 1 minute.`)
-        } else if (secondsToNextPhase === 60) {
-          sendNotification('Break ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 1 minute.`)
-        } else if (secondsToNextPhase === 5) {
-          tickingSound.play()
-        }
+        sendNotification('Hope you are well rested now!', `It's time to go at it again.`)
       }
-    },
-    secondsLeft === 0 ? null : 1000
-  )
+
+      setRunningPhase((state) => state + 1)
+
+      alarmSound.play()
+    } else {
+      setSecondsLeft(secondsToNextPhase)
+
+      if (currentPhase === Phase.Pomodoro && secondsToNextPhase === 300) {
+        sendNotification('Pomodoro ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 5 minutes.`)
+      } else if (currentPhase === Phase.Pomodoro && secondsToNextPhase === 60) {
+        sendNotification('Pomodoro ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 1 minute.`)
+      } else if (secondsToNextPhase === 60) {
+        sendNotification('Break ending soon!', `A ${nextPhase.toLowerCase()} is coming next in 1 minute.`)
+      } else if (secondsToNextPhase === 5) {
+        tickingSound.play()
+      }
+    }
+  }
 
   const formattedTimeLeft = formatSecondsToMinutesAndSeconds(secondsLeft)
 
