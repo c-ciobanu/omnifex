4e6fb4d22e23223433d1bd1558d6882d331380e1
Cristi Ciobanu <c-ciobanu@protonmail.com>
2025-02-04 20:57:38 +0100

    add script to start cron jobs

---
+++ scripts/startCronJob.ts
@@ -0,0 +1,36 @@
+import { db } from 'api/src/lib/db'
+
+const cronJobs = {
+  CheckShowChangesJob: { name: 'CheckShowChangesJob', path: 'CheckShowChangesJob/CheckShowChangesJob' },
+}
+
+interface Args {
+  job: keyof typeof cronJobs
+}
+
+export default async ({ args }: { args: Args }) => {
+  if (!args.job) {
+    throw new Error('job arg required')
+  }
+
+  const job = cronJobs[args.job]
+
+  if (!job) {
+    throw new Error('cron job not found')
+  }
+
+  const count = await db.backgroundJob.count({ where: { handler: { contains: job.name } } })
+
+  if (count !== 0) {
+    throw new Error('cron job already running')
+  }
+
+  await db.backgroundJob.create({
+    data: {
+      handler: `{"name":"${job.name}","path":"${job.path}","args":[]}`,
+      queue: 'default',
+      priority: 50,
+      runAt: new Date(),
+    },
+  })
+}
