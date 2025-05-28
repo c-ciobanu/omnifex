db66a92385771cf59f689bb2a6b3f22dc5b20464
Cristi Ciobanu <c-ciobanu@protonmail.com>
2025-05-28 18:25:59 +0200

    migrate edit document form to new form components

--- web/src/pages/Documents/DocumentsPage/DocumentsCell/EditDocumentModal/EditDocumentModal.tsx
+++ web/src/pages/Documents/DocumentsPage/DocumentsCell/EditDocumentModal/EditDocumentModal.tsx
@@ -1,10 +1,10 @@
 import { DocumentsQuery, UpdateDocumentTitleMutation, UpdateDocumentTitleMutationVariables } from 'types/graphql'
 
-import { Form, SubmitHandler } from '@redwoodjs/forms'
+import { SubmitHandler } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
 
-import { FormField, FormInput, FormSwitch } from 'src/components/OldForm/OldForm'
-import { Button } from 'src/components/ui/button'
+import { Form, FormSubmit } from 'src/components/form'
+import { FormInput, FormSwitch } from 'src/components/form/elements'
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from 'src/components/ui/dialog'
 
 const UPDATE_DOCUMENT = gql`
@@ -52,20 +52,21 @@ const EditDocumentModal = (props: EditDocumentModalProps) => {
             <DialogTitle>Edit Document</DialogTitle>
           </DialogHeader>
 
-          <FormField name="title" label="Title">
-            <FormInput name="title" defaultValue={document.title} validation={{ required: true }} />
-          </FormField>
+          <FormInput name="title" label="Title" defaultValue={document.title} validation={{ required: true }} />
 
-          <FormField name="isPublic" description="Anyone on the Internet with the link will be able to view it.">
-            <FormSwitch name="isPublic" label="Public Access" defaultValue={document.isPublic} />
-          </FormField>
+          <FormSwitch
+            name="isPublic"
+            label="Public Access"
+            description="Anyone on the Internet with the link will be able to view it."
+            defaultChecked={document.isPublic}
+          />
 
           <DialogFooter>
             <DialogClose>Close</DialogClose>
 
-            <Button type="submit" disabled={loading}>
+            <FormSubmit disabled={loading} className="w-auto">
               Save
-            </Button>
+            </FormSubmit>
           </DialogFooter>
         </Form>
       </DialogContent>
