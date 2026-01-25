5adde73780f81c9cd1b8872d7e0217b99ca26088
Cristi Ciobanu <c-ciobanu@protonmail.com>
2026-01-25 16:33:55 +0100

    show chapters left to read

--- apps/web/src/components/mangas-reading-list.tsx
+++ apps/web/src/components/mangas-reading-list.tsx
@@ -2,6 +2,7 @@ import type { OrpcClientOutputs } from "@/utils/orpc";
 import { orpc } from "@/utils/orpc";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
+import { BookOpenIcon } from "lucide-react";
 
 import { Spinner } from "./ui/spinner";
 
@@ -12,28 +13,39 @@ interface MangasGridProps {
 function MangasGrid({ mangas }: MangasGridProps) {
   return (
     <ul className="grid grid-cols-2 gap-6 sm:grid-cols-4 lg:grid-cols-6">
-      {mangas.map((manga) => (
-        <li key={manga.id}>
-          <Link
-            to="/mangas/$mangaDexId"
-            params={{ mangaDexId: manga.mangaDexId }}
-            title={manga.title}
-            className="group relative"
-          >
-            <img src={manga.coverUrl} alt={`${manga.title} cover`} className="h-full w-full" />
+      {mangas.map((manga) => {
+        const lastChapterRead = manga.userProgress.lastChapterRead ?? 0;
 
-            <div className="absolute inset-0 bg-linear-to-t from-black/70 to-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-              <div className="absolute top-2 left-2 text-sm text-white">
-                <p>{manga.title}</p>
+        const chaptersLeftToRead = manga.chapters - lastChapterRead;
 
-                <p>Next: {(manga.userProgress.lastChapterRead ?? 0) + 1}</p>
+        return (
+          <li key={manga.id}>
+            <Link
+              to="/mangas/$mangaDexId"
+              params={{ mangaDexId: manga.mangaDexId }}
+              title={manga.title}
+              className="group relative"
+            >
+              <img src={manga.coverUrl} alt={`${manga.title} cover`} className="h-full w-full" />
 
-                <p>Last: {manga.chapters}</p>
+              <div className="absolute top-2 right-2 flex items-center gap-1.5 rounded-sm bg-black/50 px-1.5 py-0.5 text-sm text-white backdrop-blur-sm">
+                <BookOpenIcon className="size-4" />
+                {chaptersLeftToRead}
               </div>
-            </div>
-          </Link>
-        </li>
-      ))}
+
+              <div className="absolute inset-0 bg-linear-to-t from-black/70 to-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                <div className="absolute top-2 left-2 text-sm text-white">
+                  <p>{manga.title}</p>
+
+                  <p>Next: {lastChapterRead + 1}</p>
+
+                  <p>Last: {manga.chapters}</p>
+                </div>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
